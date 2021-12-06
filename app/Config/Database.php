7f40325ebba0443a4146c5a254037ecc758b32ce
Simen Sundbø <sim123@live.no>
2021-12-06 15:33:41 +0100

    Ny versjon og ny database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => '127.0.0.1',
         'username' => 'root',
         'password' => '',
-        'database' => 'neo',
+        'database' => 'neodb',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
