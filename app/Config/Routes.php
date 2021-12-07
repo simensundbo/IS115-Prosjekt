2de69263160768772a6b6aab5814cf42924edc50
RikkeSolvang <rikke_solvang@hotmail.com>
2021-12-07 14:24:56 +0100

    filtrering av medlemmer utifra interesse

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -59,6 +59,9 @@ $routes->get('/addInterestView/(:num)', 'InterestController::index/$1');
 $routes->post('addInterest/(:num)' , 'InterestController::addInterest/$1');
 $routes->get('/mailDashboardView', "MailController::mailView");
 $routes->get('/sendMailView', "MailController::sendMail");
+$routes->get('/filterInterestView', "InterestController::filterInterestsView");
+$routes->post('/filterInterest', "InterestController::filterInterests");
+
 
 /*
  * --------------------------------------------------------------------
